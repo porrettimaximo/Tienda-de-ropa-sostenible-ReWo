2a152fca71e4283093ae196339285254358b1136
Maxi <porrettimaximo@gmail.com>
2026-04-17 21:30:29 -0300

    ajuste en requeriments

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,13 +36,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:4173",
-        "http://127.0.0.1:4173",
-        "https://maximoporrettiecowear.netlify.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
