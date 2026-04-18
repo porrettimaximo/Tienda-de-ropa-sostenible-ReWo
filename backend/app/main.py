bf9407ed50848e75ed837b16b7789d5a5f630aea
Maxi <porrettimaximo@gmail.com>
2026-04-17 21:33:57 -0300

    Fix CORS credentials compatibility

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,7 +37,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"]
